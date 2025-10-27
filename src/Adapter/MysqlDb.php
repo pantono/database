e5e1ab5d15b703dfeb32d7e463ef03a69a6764ec
Chris Burton <csburton@gmail.com>
2025-10-27 13:10:22 +0000

    ADding missing override

--- src/Adapter/MysqlDb.php
+++ src/Adapter/MysqlDb.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Pantono\Database\Adapter;
 
+use Pantono\Database\Query\Select\Select;
+use Pantono\Database\Query\Select\DriverSpecific\MysqlSelect;
+
 class MysqlDb extends Db
 {
+    public function select(): Select
+    {
+        return new MysqlSelect();
+    }
 }
