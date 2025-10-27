e2ee9a50a72ad5614ce2dfaa40cc7419532fc86c
Chris Burton <csburton@gmail.com>
2025-10-27 13:14:29 +0000

    Adding mssql specific return stype

--- src/Repository/MssqlRepository.php
+++ src/Repository/MssqlRepository.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Pantono\Database\Repository;
 
+use Pantono\Database\Adapter\MssqlDb;
+
 abstract class MssqlRepository extends AbstractPdoRepository
 {
+    public function getDb(): MssqlDb
+    {
+        /**
+         * @var MssqlDb $db
+         */
+        $db = $this->db;
+        return $db;
+    }
 }
