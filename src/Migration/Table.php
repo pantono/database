dd3b31fb245886674790214c15b936a59d0e2a83
Chris Burton <csburton@gmail.com>
2026-01-22 11:43:06 +0000

    Proper return types

--- src/Migration/Table.php
+++ src/Migration/Table.php
@@ -6,9 +6,10 @@ use Phinx\Db\Table as PhinxTable;
 
 class Table extends PhinxTable
 {
-    public function addLinkedColumn(string $columnName, string $linkedTable, string $linkedColumn, array $columnOptions = [], array $keyOptions = []): void
+    public function addLinkedColumn(string $columnName, string $linkedTable, string $linkedColumn, array $columnOptions = [], array $keyOptions = []): self
     {
         $this->addColumn($columnName, 'integer', $columnOptions);
         $this->addForeignKey($columnName, $linkedTable, $linkedColumn, $keyOptions);
+        return $this;
     }
 }
