34b377e906775b8edd26d93c981f856b74b3d094
Chris Burton <csburton@gmail.com>
2026-01-22 11:49:34 +0000

    Delete by default

--- src/Migration/Table.php
+++ src/Migration/Table.php
@@ -9,6 +9,9 @@ class Table extends PhinxTable
     public function addLinkedColumn(string $columnName, string $linkedTable, string $linkedColumn, array $columnOptions = [], array $keyOptions = []): self
     {
         $this->addColumn($columnName, 'integer', $columnOptions);
+        if (!isset($keyOptions['delete']) || !in_array($keyOptions['delete'], ['CASCADE', 'SET NULL', 'NO ACTION'])) {
+            $keyOptions['delete'] = 'CASCADE';
+        }
         $this->addForeignKey($columnName, $linkedTable, $linkedColumn, $keyOptions);
         return $this;
     }
