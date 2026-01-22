5c80bffdbb5a84d27f746854974d94b21b933c94
Chris Burton <csburton@gmail.com>
2026-01-22 11:34:40 +0000

    Adding helper method for linked columns

--- src/Migration/Base/BasePantonoMigration.php
+++ src/Migration/Base/BasePantonoMigration.php
@@ -5,9 +5,16 @@ namespace Pantono\Database\Migration\Base;
 use Phinx\Migration\AbstractMigration;
 use Pantono\Database\Migration\Traits\DisableForeignKeyChecksTrait;
 use Pantono\Database\Migration\Traits\ReseedIdentityTrait;
+use Phinx\Db\Table;
 
 class BasePantonoMigration extends AbstractMigration
 {
     use DisableForeignKeyChecksTrait;
     use ReseedIdentityTrait;
+
+    public function addLinkedColumn(Table $table, string $columnName, string $linkedTable, string $linkedColumn): void
+    {
+        $table->addColumn($columnName, 'integer')
+            ->addForeignKey($columnName, $linkedTable, $linkedColumn);
+    }
 }
