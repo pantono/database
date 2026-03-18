7fe1f5af35b0d91cddd06ff3b58cf81e157d2d35
Chris Burton <csburton@gmail.com>
2026-03-18 22:09:33 +0000

    Adding heloer for json queries

--- src/Query/PantonoQueryBuilder.php
+++ src/Query/PantonoQueryBuilder.php
@@ -3,9 +3,20 @@
 namespace Pantono\Database\Query;
 
 use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 
 class PantonoQueryBuilder extends QueryBuilder
 {
+    private Connection $doctrineConnection;
+
+    public function __construct(Connection $connection)
+    {
+        parent::__construct($connection);
+        $this->doctrineConnection = $connection;
+    }
+
     public function whereParam(string $expression, string|array|int $value): self
     {
         $paramName = uniqid('param');
@@ -13,5 +24,46 @@ class PantonoQueryBuilder extends QueryBuilder
         $this->where($expression)
             ->setParameter($paramName, $value);
         return $this;
+    }
+
+    public function jsonWhere(string $jsonColumn, array $jsonParts, string|int|null $value): self
+    {
+        $platform = $this->getConnection()->getDatabasePlatform();
+        if ($platform instanceof AbstractMySQLPlatform) {
+            $jsonPath = '$.' . implode('.', array_map(
+                    static fn(string $p) => str_replace('"', '\"', $p),
+                    $jsonParts
+                ));
+            $expr = sprintf(
+                "JSON_UNQUOTE(JSON_EXTRACT(%s, %s)) = :%s",
+                $jsonColumn,
+                $this->getConnection()->quote($jsonPath),
+                $value
+            );
+            $this->where($expr);
+        } elseif ($platform instanceof PostgreSQLPlatform) {
+            $pgPath = '{' . implode(',', array_map(
+                    static fn(string $p) => str_replace(['\\', '"'], ['\\\\', '\\"'], $p),
+                    $jsonParts
+                )) . '}';
+
+            $expr = sprintf(
+                "%s #>> %s = :%s",
+                $jsonColumn,
+                $this->getConnection()->quote($pgPath),
+                $value
+            );
+
+            $this->andWhere($expr);
+        } else {
+            throw new \Exception('Unsupported database platform for json queries');
+        }
+
+        return $this;
+    }
+
+    public function getConnection(): Connection
+    {
+        return $this->doctrineConnection;
     }
 }
