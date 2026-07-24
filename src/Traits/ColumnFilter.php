1e7ca85c47e10c65d3efdc0740c199fc63b9dab1
Chris Burton <csburton@gmail.com>
2026-07-24 15:05:07 +0100

    ADding helper method for applying to query builder

--- src/Traits/ColumnFilter.php
+++ src/Traits/ColumnFilter.php
@@ -2,11 +2,13 @@
 
 namespace Pantono\Database\Traits;
 
+use Doctrine\DBAL\Query\QueryBuilder;
+
 trait ColumnFilter
 {
     private array $columns = [];
 
-    public function addColumn(string $column, string|array|int $value, string $operator = '='): void
+    public function addColumn(string $column, string|array|int|null $value, string $operator = '='): void
     {
         $operator = strtoupper($operator);
         $allowedOperators = ['=', '>', '<', '>=', '<=', 'LIKE', 'NOT LIKE'];
@@ -16,9 +18,9 @@ trait ColumnFilter
         if (!in_array($operator, $allowedOperators)) {
             throw new \RuntimeException('Invalid operator');
         }
-        $placeholder = '?';
+        $placeholder = ':param_' . uniqid(true);
         if ($operator === 'IN' || $operator === 'NOT IN') {
-            $placeholder = '(?)';
+            $placeholder = '(' . $placeholder . ')';
         }
         $this->columns[] = [
             'name' => $column,
@@ -31,5 +33,39 @@ trait ColumnFilter
     public function getColumns(): array
     {
         return $this->columns;
+    }
+
+    public function applyColumnsToQueryBuilder(QueryBuilder $queryBuilder): void
+    {
+        $expressionBuilder = $queryBuilder->expr();
+
+        foreach ($this->getColumns() as $column) {
+            if ($column['value'] === null) {
+                $operator = $column['operator'];
+                if ($operator === '=') {
+                    $queryBuilder->andWhere($expressionBuilder->isNull($column['name']));
+                    continue;
+                }
+                if ($operator === '<>') {
+                    $queryBuilder->andWhere($expressionBuilder->isNotNull($column['name']));
+                    continue;
+                }
+            }
+
+            $expression = match ($column['operator']) {
+                '=' => $expressionBuilder->eq($column['name'], $column['placeholder']),
+                '>' => $expressionBuilder->gt($column['name'], $column['placeholder']),
+                '<' => $expressionBuilder->lt($column['name'], $column['placeholder']),
+                '>=' => $expressionBuilder->gte($column['name'], $column['placeholder']),
+                '<=' => $expressionBuilder->lte($column['name'], $column['placeholder']),
+                'LIKE' => $expressionBuilder->like($column['name'], $column['placeholder']),
+                'NOT LIKE' => $expressionBuilder->notLike($column['name'], $column['placeholder']),
+                'IN' => $expressionBuilder->in($column['name'], $column['placeholder']),
+                'NOT IN' => $expressionBuilder->notIn($column['name'], $column['placeholder'])
+            };
+
+            $queryBuilder->andWhere($expression)
+                ->setParameter(trim($column['placeholder'], '():'), $column['value']);
+        }
     }
 }
